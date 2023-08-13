d574c05c796093bf6914da39db5380fc08463f7a
Vinayak-0202 <vpnikam02@gmail.com>
2023-08-13 19:05:40 +0530

    Included the security pakages helmet,cors,xss-clean,express-rate-limiter

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const express = require("express");
 const authenticateUser = require("./middleware/authentication");
 const app = express();
 
+//extra security pakages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 //connectDB
 const connectDB = require("./db/connect");
 
@@ -15,7 +21,17 @@ const jobRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100, //limit each IP to 100 request per window
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // extra packages
 
 // routes
